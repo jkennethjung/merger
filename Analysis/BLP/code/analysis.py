d3331d6ba48f9516cf2b87d5e7551df09c97a714
Ken <jkjung@outlook.com>
2020-10-10 15:33:20 -0400

    BLP instruments + box constraints -> constraints violated, weird results

--- Analysis/BLP/code/analysis.py
+++ Analysis/BLP/code/analysis.py
@@ -34,7 +34,7 @@ product_data['firm_ids'] = product_data['product_ids']
 short_df = product_data[['firm_ids', 'market_ids', 'quality', 'satellite', 'wired']].head(8)
 print(short_df)
 n_ZD = 1
-demand_instruments = pyblp.build_differentiation_instruments(pyblp.Formulation('0 + quality'), product_data, version = 'quadratic')
+demand_instruments = pyblp.build_blp_instruments(pyblp.Formulation('0 + quality'), product_data)
 print(demand_instruments[0:10,:])
 
 # own characteristics will be collinear with X1 because each firm only has one 
@@ -88,7 +88,6 @@ print(optim_results.sigma_squared_se)
 
 # #### (b) When estimating jointly with supply
 
-'''
 # product_formulation
 X3_formulation = pyblp.Formulation('1 + obs_cost')
 product_formulations = (X1_formulation, X2_formulation, X3_formulation)
@@ -108,7 +107,6 @@ print(supply_results.sigma_squared)
 print("Standard errors: ")
 print(supply_results.sigma_squared_se)
 
-'''
 '''
 # update the results with optimal instruments
 instrument_results = results_supply.compute_optimal_instruments(method='approximate')
